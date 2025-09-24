39c0ca76786c7afa077de59fb1bfe2248343ad62
Alexandra Korenkova <alex.korenkova@cobweb.work>
2025-09-25 01:28:46 +0300

    add deploy.yml for github action

--- components/yandexReviews.tsx
+++ components/yandexReviews.tsx
@@ -2,11 +2,12 @@
 
 export default function YandexReviews() {
   return (
-    <div className="w-full h-[450px]">
+    <div className="w-full h-[700px] md:h-[450px] ">
       <iframe
         src="https://yandex.ru/maps-reviews-widget/2630207706?comments"
         style={{ width: "100%", height: "100%", border: "none" }}
         loading="lazy"
+        title="Отзывы о 3SAtelier на Яндекс.Картах"
       />
     </div>
   )
